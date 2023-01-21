7bf700103880dface21d1be1da294784f5b2cfe5
psu6510110225 <tang82700@gmail.com>
2023-01-21 18:07:56 +0700

    เพิ่มการauthก่อนเรียกใช้api

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,8 +1,12 @@
 import Router from "koa-router";
 import announcement from './announcement'
+import userResult from './user_result'
+import { authMiddleware } from "auth";
+
 const api = new Router()
 
-api.use('/api/announcement',announcement.routes())
+api.use('/api/announcement',authMiddleware,announcement.routes())
+api.use('/api/userResult',authMiddleware,userResult.routes())
 
 export default api
 
