d08ed9c83cba15daff45417a141d58c2eb435b6e
psu6510110225 <tang82700@gmail.com>
2023-01-22 17:12:15 +0700

    แก้การแสดงผลหน้าเว็บ

---
+++ client-ts/src/App.tsx
@@ -0,0 +1,41 @@
+import Repo from './repositories'
+import React ,{ useEffect ,useState } from 'react';
+import Announcement from './model/Annoucement';
+
+function App() {
+  const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
+
+  const fetchannouncementList = async () => {
+    const result = await Repo.announcements.getAll()
+    if(result){
+      setAnnouncementList(result)
+    }
+  }
+
+  useEffect(() => {
+    fetchannouncementList()
+  })
+
+  return (
+    <div>
+      {announcementList.map(announcement => (
+        <div key={announcement.id}>
+          <p>ID : {announcement.id}</p>
+          <p>Description : {announcement.description}</p>
+          <p>Positive : {announcement.remarkIfPositive}</p>
+          <p>Negative : {announcement.remarkIfNegative}</p>
+          <p>Time : {announcement.pubDateTime.toLocaleString('en-US', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit'
+            })}</p>
+          <p>UserCode : {announcement.userCode}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default App;
